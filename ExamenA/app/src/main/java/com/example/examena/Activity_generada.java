319d1d3a318fdd61e169d4ba9a43ca786463c01e
Alexandra-rangelo <aletommo99@gmail.com>
2024-06-14 13:13:32 -0600

    moficiación examen, esta sí es jaja

--- ExamenA/app/src/main/java/com/example/examena/Activity_generada.java
+++ ExamenA/app/src/main/java/com/example/examena/Activity_generada.java
@@ -7,30 +7,37 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.ArrayList;
 import java.util.List;
 
-//mostrar una lista de números en un RecyclerView en una cuadrícula de 4 columnas
 public class Activity_generada extends AppCompatActivity {
     private RecyclerView recyclerView;
     private Adaptador adaptador;
-    private static final int NUM_COLUMNS = 4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_generada);
 
-        recyclerView = findViewById(R.id.recyclerView); //Asigna el RecyclerView del layout a la variable recyclerView utilizando su ID.
-        //Configura el RecyclerView para usar un GridLayoutManager con el número de columnas especificado por NUM_COLUMNS
-        recyclerView.setLayoutManager(new GridLayoutManager(this, NUM_COLUMNS));
+        recyclerView = findViewById(R.id.recyclerView);
 
         int numero = getIntent().getIntExtra("numero", 0);
+        int columns, rows;
+        double sqrt = Math.sqrt(numero);
 
-        // Calcula el número de elementos que llena completamente las filas de cuatro columnas
-        int numberOfItemsToShow = (numero / NUM_COLUMNS) * NUM_COLUMNS;
+        if (sqrt == (int) sqrt) {  // Si la raíz cuadrada es un número entero
+            columns = (int) sqrt;
+            rows = (int) sqrt;
+        } else {  // Si la raíz cuadrada no es un número entero (un decimal jaja)
+            columns = (int) Math.floor(sqrt);
+            rows = columns;
+        }
 
-        //Crea una nueva lista de enteros para almacenar los elementos que se mostrarán en el RecyclerView.
+        recyclerView.setLayoutManager(new GridLayoutManager(this, columns));
+
+        // Crear una lista de elementos que incluirá números del 1 hasta el número máximo que cabe en el cuadrado
         List<Integer> items = new ArrayList<>();
-        for (int i = 1; i <= numberOfItemsToShow; i++) {
-            items.add(i); //Agrega el valor actual de i a la lista items
+        int totalItems = columns * rows;
+
+        for (int i = 1; i <= totalItems; i++) {
+            items.add(i);
         }
 
         adaptador = new Adaptador(items);
